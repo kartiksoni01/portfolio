99ca3e404b94aeda4c37b61b3eed27656aebacd5
kartikPC <kartiksoni010@gmail.com>
2024-01-24 14:18:04 +0530

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,29 +104,32 @@ function showSkills(skills) {
     skillsContainer.innerHTML = skillHTML;
 }
 
+
+
 function showProjects(projects) {
     let projectsContainer = document.querySelector(".work .box-container");
     let projectsHTML = "";
     projects.forEach(project => {
         projectsHTML += `
-        <div class="grid-item ${project.category}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
-      <div class="content">
+        <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+        <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
         </div>
         <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
+            <p>${project.desc}</p>
+            <div class="btns">
             <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-          </div>
+            </div>
         </div>
-      </div>
+        </div>
     </div>
     </div>`
     });
     projectsContainer.innerHTML = projectsHTML;
+
+
 
     // <!-- tilt js effect starts -->
     VanillaTilt.init(document.querySelectorAll(".tilt"), {
